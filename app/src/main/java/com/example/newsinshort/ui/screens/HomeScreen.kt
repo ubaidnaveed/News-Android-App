466cea26e8ef6b548356d085a90ba78009e40aab
ubaidnaveed <ubaidub97@gmail.com>
2024-11-28 13:00:39 +0500

    Implementing custom composables.

--- app/src/main/java/com/example/newsinshort/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/newsinshort/ui/screens/HomeScreen.kt
@@ -1,21 +1,44 @@
 package com.example.newsinshort.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.newsinshort.ui.components.Loader
 import com.example.newsinshort.ui.viewmodel.NewsViewModel
+import com.example.utilities.ResourceState
+
+const val TAG = "HomeScreen"
 
 @Composable
 fun HomeScreen(
-    NewsViewModel : NewsViewModel = hiltViewModel()
+    newsViewModel : NewsViewModel = hiltViewModel()
 ){
+
+    val newsRes by newsViewModel.news.collectAsState()
+
     Surface(
         modifier = Modifier.fillMaxSize()
     ) {
-
+        when(newsRes){
+            is ResourceState.Loading -> {
+                Log.d(TAG, "Inside loading")
+                Loader()
+            }
+            is ResourceState.Success -> {
+                val response = (newsRes as ResourceState.Success).data
+                Log.d(TAG, "Inside Success")
+            }
+            is ResourceState.Error -> {
+                val error = (newsRes as ResourceState.Error)
+                Log.d(TAG, "Inside error $error")
+            }
+        }
     }
 }
 
